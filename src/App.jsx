2b24bf366f3694918c0ea60271b4228bb0dd3ef2
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-11-21 18:51:01 +0600

    check

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        {/* <Route path="/" element={<Home />}></Route> */}
+        <Route path="/" element={<Home />}></Route>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}></Route>
         </Route>
