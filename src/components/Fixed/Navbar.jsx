8171b2ee7cd1ebf67530db6f0d0dcb1e47f84c6b
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-12-02 18:38:18 +0600

    start now

--- src/components/Fixed/Navbar.jsx
+++ src/components/Fixed/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 const Navbar = () => {
   return <div>
-    <div className="container"></div>
+    <div className="container">
+      <div>logo</div>
+    </div>
   </div>;
 };
 
